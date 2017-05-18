53ff560cfab9f06dfad65aee82f53c63a2f2030e
Jonathan Peralez <jperalez@gmail.com>
2017-05-17 18:49:15 -0700

    Just getting started

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,9 +10,5 @@
 var product = require('../models/product');
 
 exports.product_list = function(req, res) {
-	// res.render('products', {productName: products[0].name, productPrice: products[0].price});
 	res.render('productTemplate');
-	// products.forEach(function(product) {
-	// 	res.render('product_list', {name: product.name, price: product.price});
-	// });
 };
