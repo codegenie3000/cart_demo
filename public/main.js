1269981db0a7fc9becf99fbbdb82b4b29e36ab92
Jonathan Peralez <jperalez@gmail.com>
2017-07-14 16:27:32 -0700

    Placed code in document.ready

--- public/main.js
+++ public/main.js
@@ -7,50 +7,52 @@
  * Created by Jonathan on 6/19/2017.
  */
 
-$('.change-qty').change(function (e) {
-	e.preventDefault();
-	var qty = parseInt(this.value);
-	var refId = this.id;
-	var itemId = function() {
-		var re  = /qtySelectItem(\S*)/;
-		return re.exec(refId)[1];
-	}();
-	var obj = {
-		itemId: itemId,
-		qty: qty
-	};
-	$.post('/cart/change_qty', {
-		data: JSON.stringify(obj),
-		dataType: 'json',
-		success: function(data) {
-			debugger;
-			console.log(data);
-			// goToCart();
-			window.location = '/cart/';
-		}
+$(document).ready(function() {
+	$('.change-qty').change(function (e) {
+		e.preventDefault();
+		var qty = parseInt(this.value);
+		var refId = this.id;
+		var itemId = function() {
+			var re  = /qtySelectItem(\S*)/;
+			return re.exec(refId)[1];
+		}();
+		var obj = {
+			itemId: itemId,
+			qty: qty
+		};
+		$.post('/cart/change_qty', {
+			data: JSON.stringify(obj),
+			dataType: 'json',
+			success: function(data) {
+				debugger;
+				console.log(data);
+				// goToCart();
+				window.location = '/cart/';
+			}
+		});
 	});
+	
+	$(':button').click(function(e) {
+		console.log('clicked');
+		this.blur();
+		window.location = '/cart/checkout01';
+	});
+	
+	/*
+	 $('.delete-button').click(function(e) {
+	 e.preventDefault();
+	 var re = /delete(\S*)/;
+	 var id = re.exec(this.id)[1];
+	 /!*debugger;
+	 e.preventDefault();
+	 var re = /delete(\S*)/;
+	 var id = re.exec(this.id)[1];
+	 $.ajax('/cart/delete_product/', {
+	 data: id,
+	 dataType: 'text',
+	 success: function (data) {
+	 console.log(data);
+	 }
+	 });*!/
+	 });*/
 });
-
-$(':button').click(function(e) {
-	console.log('clicked');
-	this.blur();
-	window.location = '/cart/checkout01';
-});
-
-/*
-$('.delete-button').click(function(e) {
-	e.preventDefault();
-	var re = /delete(\S*)/;
-	var id = re.exec(this.id)[1];
-	/!*debugger;
-	e.preventDefault();
-	var re = /delete(\S*)/;
-	var id = re.exec(this.id)[1];
-	$.ajax('/cart/delete_product/', {
-		data: id,
-		dataType: 'text',
-		success: function (data) {
-			console.log(data);
-		}
-	});*!/
-});*/
