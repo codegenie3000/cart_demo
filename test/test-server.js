e25098ac526fab2bbbca50196b63cfed16d61349
Jonathan Peralez <jperalez@gmail.com>
2017-09-14 09:09:19 -0700

    End to end testing of routes. Figuring out how to test stripe

--- test/test-server.js
+++ test/test-server.js
@@ -5,14 +5,16 @@
 
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../app');
+var app = require('../app');
 var should = chai.should();
 
 chai.use(chaiHttp);
 
+var agent = chai.request.agent(app);
+
 describe('/GET index', function() {
     it('should return a 200', function(done) {
-        chai.request(server)
+        agent
             .get('/')
             .end(function(err, res) {
                 res.should.have.status(200);
@@ -23,7 +25,7 @@ describe('/GET index', function() {
 
 describe('/GET about', function() {
     it('should return a 200', function(done) {
-        chai.request(server)
+        agent
             .get('/about')
             .end(function(err, res) {
                 res.should.have.status(200);
@@ -34,8 +36,19 @@ describe('/GET about', function() {
 
 describe('/GET product', function() {
     it('should return a 200', function(done) {
-        chai.request(server)
+        agent
             .get('/product/5930ab5b3488d916dc515420')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe('/POST add product to cart', function() {
+    it('should return /cart', function(done) {
+        agent
+            .post('/product/5930ab5b3488d916dc515420')
             .end(function(err, res) {
                 res.should.have.status(200);
                 done();
@@ -45,8 +58,109 @@ describe('/GET product', function() {
 
 describe('/GET cart', function() {
     it('should return a 200', function(done) {
-        chai.request(server)
+        agent
             .get('/cart')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe('/PUT cart - change qty', function() {
+    it('should return a 200', function(done) {
+        agent
+            .put('/cart/change_qty')
+            .send({'itemId': '5930ab5b3488d916dc515420', 'qty': '5'})
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.text.should.equal('/cart');
+                done();
+            });
+    });
+});
+
+describe('/GET billing address', function() {
+    it('should return a 200', function(done) {
+        agent
+            .get('/cart/billing')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe('/POST billing address', function() {
+    it('should return a 200', function(done) {
+        agent
+            .post('/cart/billing/submit')
+            .send({
+                'name': 'Jonathan Peralez',
+                'email:': 'me@jonathanperalez.com',
+                'address1': '123 Main St.',
+                'address2': 'Apt 1',
+                'city': 'Los Angeles',
+                'state': 'CA',
+                'zip:': '12345',
+                'phone': '5555555555',
+                'bill-same-as-ship': false
+            })
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.text.should.equal('/cart/shipping');
+                done();
+            });
+    });
+});
+
+describe('/GET shipping address', function() {
+    it('should return a 200', function(done) {
+        agent
+            .get('/cart/shipping')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe('/POST shipping address', function() {
+    it('should return a 200 and return text', function(done) {
+        agent
+            .post('/cart/shipping/submit')
+            .send({
+                'name': 'Jonathan Peralez',
+                'address1': '123 Main St.',
+                'address2': 'Apt 1',
+                'city': 'Los Angeles',
+                'state': 'CA',
+                'zip': '12345',
+                'phone': '5555555555'
+            })
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.text.should.equal('/cart/confirmation');
+                done();
+            });
+    });
+});
+
+describe('/GET confirmation', function() {
+    it('should return a 200', function(done) {
+        agent
+            .get('/cart/confirmation')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+describe('/payment', function() {
+    it('should return a 200', function(done) {
+        agent
+            .get('/cart/payment')
             .end(function(err, res) {
                 res.should.have.status(200);
                 done();
