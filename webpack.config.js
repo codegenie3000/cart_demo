23622ed24f0f60f164534bdd24e9970e127c6534
Jonathan Peralez <jperalez@gmail.com>
2017-12-03 12:17:07 -0800

    Updated webpack to use es2015 preset and added source map to devtool

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,20 @@ const path = require('path');
 
 module.exports = {
     entry: './src/main.js',
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query:  {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'public')
-    }
+    },
+    devtool: 'source-map'
 };
 
