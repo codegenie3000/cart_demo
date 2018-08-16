d2d6a754d5449cffbbd318ad08eab05dbcfa49c1
Jonathan Peralez <jperalez@gmail.com>
2018-08-15 17:12:02 -0700

    Making progress on updating gulp migration to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+/*
 const gulp = require('gulp');
 
 gulp.task('watch', () => {
@@ -10,6 +11,58 @@ gulp.task('css', () => {
 });
 
 gulp.task('publicImages', () => {
-    return gulp.src('src/*.jpeg')
+    return gulp.src('src/!*.jpeg')
+        .pipe(gulp.dest('./public/'));
+});*/
+
+const {series, parallel, src, dest} = require('gulp');
+
+const gulp = require('gulp');
+
+function defaultTask(cb) {
+    cb();
+}
+
+// private task
+function build(cb) {
+    cb();
+}
+
+function cssToPublic() {
+    return src('src/style.css')
+        .pipe(dest('./public/'));
+    // cb();
+}
+
+function imagesToPublic() {
+    return src('src/*.jpeg')
+        .pipe(dest('./public/'));
+}
+
+exports.build = parallel(cssToPublic, imagesToPublic);
+
+/*function watchCSS() {
+
+    cb();
+}*/
+
+// public export
+// exports.build = build;
+
+// private
+// exports.build = series(cssToPublic, imagesToPublic, watchCSS);
+
+/*
+gulp.task('watch', function() {
+    gulp.watch('src/style.css', ['css']);
+});
+
+gulp.task('css', function() {
+    return gulp.src('src/style.css')
         .pipe(gulp.dest('./public/'));
 });
+
+gulp.task('publicImages', function() {
+    return gulp.src('src/!*.jpeg')
+        .pipe(gulp.dest('./public/'));
+});*/
